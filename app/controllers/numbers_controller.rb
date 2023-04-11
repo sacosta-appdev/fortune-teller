8c375c27c8330022fbda798551e99cb8e1d0c222
Santiago Acosta <sacostal@chicagobooth.edu>
2023-04-11 14:18:57 +0000

    lottery/unlucky fully functional

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,13 +1,28 @@
-class NumbersController < ApplicationController
+class NumbersController < ApplicationController #how to define a new, custom class that's a child of the base ApplicationController
+
   def winners
     @zebra = Array.new
 
     5.times do
-      giraffe = rand(1...100)
+      giraffe = rand(1...100) #3 dots exclude the last digit from the range. 
       
       @zebra.push(giraffe)
     end
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+
+    @losing_array = Array.new
+
+    5.times do
+      rand_nbrs = rand(1...100)
+      @losing_array.push(rand_nbrs)
+    end
+
+    render({ :template => "lottery_stuff/boohoo.html.erb"})
+
+  end
+
 end
